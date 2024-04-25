c050d84f9db85dcb65df006d453a7b80cf2334d4
W. Smith <109860969+WSmith-InfoSec@users.noreply.github.com>
2024-04-25 13:30:18 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,261 @@
+function populateMiscellaneous()
+{
+	let input1 = document.getElementById('box1');
+	let input2 = document.getElementById('box2');
+	let input3 = document.getElementById('box3');
+	let input4 = document.getElementById('box4');
+	let input5 = document.getElementById('box5');
+	let input6 = document.getElementById('box6');
+	let input7 = document.getElementById('box7');
+	let input8 = document.getElementById('box8');
+  let input9 = document.getElementById('box8');
+  let input10 = document.getElementById('box8');
+  let input11 = document.getElementById('box8');
+  let input12 = document.getElementById('box8');
+  let input13 = document.getElementById('box8');
+  let input14 = document.getElementById('box8');
+  let input15 = document.getElementById('box8');
+  let input16 = document.getElementById('box8');
+  let input17 = document.getElementById('box8');
+  let input18 = document.getElementById('box8');
+  
+	
+	input1.value = "60";
+	input2.value = "20";
+	input3.value = "64";
+	input4.value = "6";
+	input5.value = "51078";
+	input6.value = "8883";
+	input7.value = "0";
+	input8.value = "0";
+  input9.value = "29200";
+  input10.value = "40";
+  input11.value = "0";
+  input12.value = "0";
+  input13.value = "0";
+  input14.value = "-1";
+  input15.value = "-1";
+  input16.value = "-1";
+  input17.value = "-1";
+  input18.value = "-1";
+  
+}
+
+function populateCamera()
+{
+	let input1 = document.getElementById('box1');
+	let input2 = document.getElementById('box2');
+	let input3 = document.getElementById('box3');
+	let input4 = document.getElementById('box4');
+	let input5 = document.getElementById('box5');
+	let input6 = document.getElementById('box6');
+	let input7 = document.getElementById('box7');
+	let input8 = document.getElementById('box8');
+  let input9 = document.getElementById('box8');
+  let input10 = document.getElementById('box8');
+  let input11 = document.getElementById('box8');
+  let input12 = document.getElementById('box8');
+  let input13 = document.getElementById('box8');
+  let input14 = document.getElementById('box8');
+  let input15 = document.getElementById('box8');
+  let input16 = document.getElementById('box8');
+  let input17 = document.getElementById('box8');
+  let input18 = document.getElementById('box8');
+  
+	
+	input1.value = "142";
+	input2.value = "20";
+	input3.value = "64";
+	input4.value = "6";
+	input5.value = "55778";
+	input6.value = "443";
+	input7.value = "3941";
+	input8.value = "149";
+  input9.value = "2529";
+  input10.value = "32";
+  input11.value = "90";
+  input12.value = "2";
+  input13.value = "0";
+  input14.value = "-1";
+  input15.value = "-1";
+  input16.value = "-1";
+  input17.value = "-1";
+  input18.value = "-1";
+  
+}
+function populateAssistant()
+{
+	let input1 = document.getElementById('box1');
+	let input2 = document.getElementById('box2');
+	let input3 = document.getElementById('box3');
+	let input4 = document.getElementById('box4');
+	let input5 = document.getElementById('box5');
+	let input6 = document.getElementById('box6');
+	let input7 = document.getElementById('box7');
+	let input8 = document.getElementById('box8');
+  let input9 = document.getElementById('box8');
+  let input10 = document.getElementById('box8');
+  let input11 = document.getElementById('box8');
+  let input12 = document.getElementById('box8');
+  let input13 = document.getElementById('box8');
+  let input14 = document.getElementById('box8');
+  let input15 = document.getElementById('box8');
+  let input16 = document.getElementById('box8');
+  let input17 = document.getElementById('box8');
+  let input18 = document.getElementById('box8');
+  
+	
+	input1.value = "52";
+	input2.value = "20";
+	input3.value = "64";
+	input4.value = "6";
+	input5.value = "43925";
+	input6.value = "443";
+	input7.value = "4046";
+	input8.value = "4628";
+  input9.value = "494";
+  input10.value = "32";
+  input11.value = "0";
+  input12.value = "12";
+  input13.value = "0";
+  input14.value = "-1";
+  input15.value = "-1";
+  input16.value = "-1";
+  input17.value = "-1";
+  input18.value = "-1";
+  
+}
+function populateOutlet()
+{
+	let input1 = document.getElementById('box1');
+	let input2 = document.getElementById('box2');
+	let input3 = document.getElementById('box3');
+	let input4 = document.getElementById('box4');
+	let input5 = document.getElementById('box5');
+	let input6 = document.getElementById('box6');
+	let input7 = document.getElementById('box7');
+	let input8 = document.getElementById('box8');
+  let input9 = document.getElementById('box8');
+  let input10 = document.getElementById('box8');
+  let input11 = document.getElementById('box8');
+  let input12 = document.getElementById('box8');
+  let input13 = document.getElementById('box8');
+  let input14 = document.getElementById('box8');
+  let input15 = document.getElementById('box8');
+  let input16 = document.getElementById('box8');
+  let input17 = document.getElementById('box8');
+  let input18 = document.getElementById('box8');
+  
+	
+	input1.value = "40";
+	input2.value = "20";
+	input3.value = "255";
+	input4.value = "6";
+	input5.value = "5794";
+	input6.value = "80";
+	input7.value = "308";
+	input8.value = "228";
+  input9.value = "1500";
+  input10.value = "20";
+  input11.value = "0";
+  input12.value = "77";
+  input13.value = "0";
+  input14.value = "-1";
+  input15.value = "-1";
+  input16.value = "-1";
+  input17.value = "-1";
+  input18.value = "-1";
+  
+}
+function populateMobile()
+{
+	let input1 = document.getElementById('box1');
+	let input2 = document.getElementById('box2');
+	let input3 = document.getElementById('box3');
+	let input4 = document.getElementById('box4');
+	let input5 = document.getElementById('box5');
+	let input6 = document.getElementById('box6');
+	let input7 = document.getElementById('box7');
+	let input8 = document.getElementById('box8');
+  let input9 = document.getElementById('box8');
+  let input10 = document.getElementById('box8');
+  let input11 = document.getElementById('box8');
+  let input12 = document.getElementById('box8');
+  let input13 = document.getElementById('box8');
+  let input14 = document.getElementById('box8');
+  let input15 = document.getElementById('box8');
+  let input16 = document.getElementById('box8');
+  let input17 = document.getElementById('box8');
+  let input18 = document.getElementById('box8');
+  
+	
+	input1.value = "83";
+	input2.value = "20";
+	input3.value = "64";
+	input4.value = "6";
+	input5.value = "52380";
+	input6.value = "443";
+	input7.value = "1714";
+	input8.value = "31608";
+  input9.value = "2048";
+  input10.value = "32";
+  input11.value = "31";
+  input12.value = "14";
+  input13.value = "0";
+  input14.value = "-1";
+  input15.value = "-1";
+  input16.value = "-1";
+  input17.value = "-1";
+  input18.value = "-1";
+  
+}
+async function runExample() {
+
+    var x = new Float32Array( 1, 18 )
+
+    var x = [];
+
+     x[0] = document.getElementById('box1').value;
+     x[1] = document.getElementById('box2').value;
+     x[2] = document.getElementById('box3').value;
+     x[3] = document.getElementById('box4').value;
+     x[4] = document.getElementById('box5').value;
+     x[5] = document.getElementById('box6').value;
+     x[6] = document.getElementById('box7').value;
+     x[7] = document.getElementById('box8').value;
+    x[8] = document.getElementById('box9').value;
+  x[9] = document.getElementById('box10').value;
+  x[10] = document.getElementById('box11').value;
+  x[11] = document.getElementById('box12').value;
+  x[12] = document.getElementById('box13').value;
+  x[13] = document.getElementById('box14').value;
+  x[14] = document.getElementById('box15').value;
+  x[15] = document.getElementById('box16').value;
+  x[16] = document.getElementById('box17').value;
+  x[17] = document.getElementById('box18').value;
+
+    let tensorX = new onnx.Tensor(x, 'float32', [1, 18]);
+
+    let session = new onnx.InferenceSession();
+
+    await session.loadModel("DLnet_IoTData.onnx");
+    let outputMap = await session.run([tensorX]);
+    let outputData = outputMap.get('output1');
+
+    let predictions = document.getElementById('predictions');
+    predictions.innerHTML = `
+  <table>
+     <tr>
+        <td> Device Type </td>
+        <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
+     </tr>
+  </table>
+  `;
+
+let result = document.getElementByID('result');
+let rating = outputData.data[0].toFixed(2);
+
+	if (rating > 0.70){
+		result.innerHTML = `<h3>Poisonous</h3>`
+	}
+}
